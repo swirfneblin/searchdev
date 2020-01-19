b0d4098aa0aa8ba2b063466b12d0531ee3195b7b
swirfneblin <swirfneblin@gmail.com>
2020-01-18 22:28:12 -0300

    api server

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
 
 const user = process.env.API_USER;
 const pass = process.env.API_PASS;
@@ -21,4 +23,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
